efeaf82001ddad87fcd903a4061f1925ee24a046
bokjo <bstojcevski89@gmail.com>
2018-05-15 00:07:43 +0200

    Templating: Added separate templates and separate handle func

--- webapp/main.go
+++ webapp/main.go
@@ -1,23 +1,39 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
-	"os"
+	"log"
+	"net/http"
 )
 
 func main() {
-	templateString := `Lemonade Stand Supply Co.`
-	//http.ListenAndServe(":1234", http.FileServer(http.Dir("../public")))
+	templates := populateTemplates()
 
-	t, err := template.New("template").Parse(templateString)
-	if err != nil {
-		fmt.Println(err)
-	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		requestedFile := r.URL.Path[1:]
 
-	err = t.Execute(os.Stdout, nil)
-	if err != nil {
-		fmt.Println(err)
-	}
+		t := templates.Lookup(requestedFile + ".html")
 
+		if t != nil {
+			err := t.Execute(w, nil)
+			if err != nil {
+				log.Println(err)
+			}
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+		}
+	})
+
+	http.Handle("/img/", http.FileServer(http.Dir("../public")))
+	http.Handle("/css/", http.FileServer(http.Dir("../public")))
+
+	http.ListenAndServe(":1234", nil)
+}
+
+func populateTemplates() *template.Template {
+	result := template.New("templates")
+	basePath := "../templates"
+
+	template.Must(result.ParseGlob(basePath + "/*.html"))
+	return result
 }
