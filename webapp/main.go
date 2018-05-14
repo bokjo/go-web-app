bb9c79fdb499f8047ed78d8a8cda6d70209e64f3
bokjo <bstojcevski89@gmail.com>
2018-05-14 22:30:13 +0200

    Simplified the FIleServer for handling static files in one line...

--- webapp/main.go
+++ webapp/main.go
@@ -1,40 +1,9 @@
 package main
 
 import (
-	"io"
-	"log"
 	"net/http"
-	"os"
-	"strings"
 )
 
-func serveStatic(w http.ResponseWriter, r *http.Request) {
-
-	f, err := os.Open("../public" + r.URL.Path)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		log.Println(err)
-	}
-	defer f.Close()
-
-	var contentType string
-
-	switch {
-	case strings.HasSuffix(r.URL.Path, "css"):
-		contentType = "text/css"
-	case strings.HasSuffix(r.URL.Path, "html"):
-		contentType = "text/html"
-	case strings.HasSuffix(r.URL.Path, "png"):
-		contentType = "image/png"
-	default:
-		contentType = "text/plain"
-	}
-	w.Header().Add("Content-Type", contentType)
-	io.Copy(w, f)
-
-}
-
 func main() {
-	http.HandleFunc("/", serveStatic)
-	http.ListenAndServe(":1234", nil)
+	http.ListenAndServe(":1234", http.FileServer(http.Dir("../public")))
 }
