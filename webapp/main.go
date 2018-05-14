aabeb319faae2b610f5b5059b6fdb3b9c9512cbf
bokjo <bstojcevski89@gmail.com>
2018-05-14 15:55:48 +0200

    Added static file handling through GO

--- webapp/main.go
+++ webapp/main.go
@@ -1,9 +1,40 @@
 package main
 
 import (
+	"io"
+	"log"
 	"net/http"
+	"os"
+	"strings"
 )
 
+func serveStatic(w http.ResponseWriter, r *http.Request) {
+
+	f, err := os.Open("../public" + r.URL.Path)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println(err)
+	}
+	defer f.Close()
+
+	var contentType string
+
+	switch {
+	case strings.HasSuffix(r.URL.Path, "css"):
+		contentType = "text/css"
+	case strings.HasSuffix(r.URL.Path, "html"):
+		contentType = "text/html"
+	case strings.HasSuffix(r.URL.Path, "png"):
+		contentType = "image/png"
+	default:
+		contentType = "text/plain"
+	}
+	w.Header().Add("Content-Type", contentType)
+	io.Copy(w, f)
+
+}
+
 func main() {
-	http.ListenAndServe(":1234", http.FileServer(http.Dir("public")))
+	http.HandleFunc("/", serveStatic)
+	http.ListenAndServe(":1234", nil)
 }
