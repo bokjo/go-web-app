f65e77b3e722b0e60f36dbadb2ab831e35e0b8ed
bokjo <bstojcevski89@gmail.com>
2018-05-20 21:42:39 +0200

    Changed the template composition and the loading process...

--- webapp/main.go
+++ webapp/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -12,16 +14,17 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		requestedFile := r.URL.Path[1:]
 
-		t := templates.Lookup(requestedFile + ".html")
+		template := templates[requestedFile+".html"]
 
-		if t != nil {
-			err := t.Execute(w, nil)
+		if template != nil {
+			err := template.Execute(w, nil)
 			if err != nil {
 				log.Println(err)
 			}
 		} else {
 			w.WriteHeader(http.StatusNotFound)
 		}
+
 	})
 
 	http.Handle("/img/", http.FileServer(http.Dir("../public")))
@@ -30,10 +33,44 @@ func main() {
 	http.ListenAndServe(":1234", nil)
 }
 
-func populateTemplates() *template.Template {
-	result := template.New("templates")
-	basePath := "../templates"
+func populateTemplates() map[string]*template.Template {
+	result := make(map[string]*template.Template)
+	const basePath = "../templates"
 
-	template.Must(result.ParseGlob(basePath + "/*.html"))
+	layout := template.Must(template.ParseFiles(basePath + "/_layout.html"))
+
+	template.Must(layout.ParseFiles(basePath+"/_header.html", basePath+"/_footer.html"))
+
+	dir, err := os.Open(basePath + "/content")
+	if err != nil {
+		panic("Cannot open the content template directory: " + err.Error())
+	}
+
+	files, err := dir.Readdir(-1)
+	if err != nil {
+		panic("Failed the read the contents of the content directory: " + err.Error())
+	}
+
+	for _, file := range files {
+		f, err := os.Open(basePath + "/content/" + file.Name())
+		if err != nil {
+			panic("Failed to open template: '" + file.Name() + "'")
+		}
+
+		content, err := ioutil.ReadAll(f)
+		if err != nil {
+			panic("Failed to read the content from the template file: '" + file.Name() + "'")
+		}
+
+		f.Close()
+
+		temp := template.Must(layout.Clone())
+		_, err = temp.Parse(string(content))
+		if err != nil {
+			panic("Failed to parse contents of '" + file.Name() + "' as template")
+		}
+		result[file.Name()] = temp
+	}
+
 	return result
 }
